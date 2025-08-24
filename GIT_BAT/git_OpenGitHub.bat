f1dfa79f500f6a2a90b7ef5c730a636ada800be1
walter <icivixwalter@gmail.com>
2025-08-24 18:32:28 +0200

    inizio

---
+++ GIT_BAT/git_OpenGitHub.bat
@@ -0,0 +1,48 @@
+
+start https://github.com/icivixwalter
+
+
+
+
+
+
+
+
+
+
+
+
+goto Utilita_git
+
+	note
+		descrizione di comandi veloci in git
+
+	C 
+		CLONARE UNA REPOSITORY 
+			esempio clono la repository  del Dos (attenzione la cartella non deve essistere nella
+			directotory dove viene effettuato il clone): 
+
+				git clone https://github.com/icivixwalter/DOS.git
+
+
+:Utilita_git
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
